cbf9a11634e7b357bcbcac442ef83da28afc6ab8
unknown <antoine.cuvilliez@outlook.fr>
2025-07-02 14:59:58 +0200

    refactor:main Adding validation seeders and unifying main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,16 @@ import { DataSource } from 'typeorm';
 import { runSeeders } from 'typeorm-extension';
 import { OrderStatusSeeder } from './database/seeders/order-status.seeder';
 import { OrdersSeeder } from './database/seeders/orders.seeder';
-// Ajoute d'autres seeders si nécessaires
+import * as fs from 'fs/promises';
+import { join } from 'path';
+import { useContainer } from 'class-validator';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   // Activer CORS avec les bonnes options
   app.enableCors({
@@ -40,34 +44,49 @@ async function bootstrap() {
 
   // Swagger configuration
   const config = new DocumentBuilder()
-    .setTitle('API Documentation')
-    .setDescription("Documentation de l'API NestJS avec Swagger")
+    .setTitle('API Order Documentation')
+    .setDescription("Documentation de l'API Order NestJS avec Swagger")
     .setVersion('1.0')
-    .addTag('App', "Point d'entrée de l'api")
-    .addServer(
-      process.env.BACKEND_DOMAIN || 'http://localhost:3005',
-      'Local dev',
-    )
+    .addServer(process.env.BACKEND_DOMAIN, 'Local dev')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  if (
-    process.env.NODE_ENV === 'development' &&
-    process.env.RUN_SEEDERS === 'true'
-  ) {
-    console.log('Running database seeders for order API...');
-    const dataSource = app.get(DataSource);
+  // Exécuter les seeders en environnement de développement si nécessaire
+  if (process.env.NODE_ENV === 'development' && process.env.RUN_SEEDERS === 'true') {
+    const seedFilePath = join(__dirname, '.seeded');
+    let hasBeenSeeded = false;
+
+    // Vérifier si le fichier .seeded existe
     try {
-      await runSeeders(dataSource, {
-        seeds: [OrderStatusSeeder, OrdersSeeder],
-      });
-      console.log('Seeders executed successfully for order API.');
-    } catch (error) {
-      console.error('Error running seeders for order API:', error);
-      throw error;
+      await fs.access(seedFilePath);
+      hasBeenSeeded = true;
+      console.log('Seeders already executed, skipping...');
+    } catch {
+      // Le fichier n'existe pas, les seeders n'ont pas encore été exécutés
+      hasBeenSeeded = false;
+    }
+
+    if (!hasBeenSeeded) {
+      console.log('Running database seeders for client API...');
+      const dataSource = app.get(DataSource);
+      try {
+        await runSeeders(dataSource, {
+          seeds: [
+            OrderStatusSeeder,
+            OrdersSeeder
+          ],
+        });
+        console.log('Seeders executed successfully for client API.');
+
+        // Créer le fichier .seeded pour marquer l'exécution
+        await fs.writeFile(seedFilePath, 'Seeded on ' + new Date().toISOString());
+      } catch (error) {
+        console.error('Error running seeders for client API:', error);
+        throw error;
+      }
     }
   }
 
